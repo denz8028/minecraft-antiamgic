b344c305b1b4a5ddf6630ef27e429b4992e94bc3
den4ik <denzel8028@hotmail.com>
2025-08-22 19:56:31 +0300

    stupid nyigga

--- src/client/java/lovelinux/den4ik/antimagic/client/mixin/InventoryMixin.java
+++ src/client/java/lovelinux/den4ik/antimagic/client/mixin/InventoryMixin.java
@@ -13,6 +13,7 @@ import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 
 @Mixin(ItemStack.class)
 public class InventoryMixin {
@@ -20,9 +21,8 @@ public class InventoryMixin {
 //    @Shadow @Final private Minecraft minecraft;
 
     @ModifyReturnValue(
-            method = {"getStyledHoverName", "getCustomName"},
-            at = @At("RETURN"),
-            remap = false
+            method = {"getStyledHoverName()Lnet/minecraft/network/chat/Component;", "getCustomName()Lnet/minecraft/network/chat/Component;"},
+            at = @At("RETURN")
     )
     private Component modifyMessage(Component original) {
         return removeObfuscatedFormatting(original);
